a550a2cc582a3584059f86a7d8ff07bddb255821
Jack Thorley <78469962+JThor1990@users.noreply.github.com>
2022-12-16 10:44:59 +0000

    Update Thorley_GroupsizeeffectsinDMR_iii_singlefemalebodycondition.R

--- R scripts/Thorley_GroupsizeeffectsinDMR_iii_singlefemalebodycondition.R
+++ R scripts/Thorley_GroupsizeeffectsinDMR_iii_singlefemalebodycondition.R
@@ -1,11 +1,11 @@
 #----------------------------------------------------------------
-# No obvious benefit of group size in wild Damaraland mole-rats Fukomys damarensis
+# Damaraland mole-rat breeders do not rely on helpers for reproduction or survival
 #
 # iii- "Body condition of single females" 
 #
 # R script
-# Authors: Jack Thorley, Hanna Bensch, Markus Zöttl
-# Contact: jack.thorley1@gmail.com
+# Authors: Jack Thorley, Hanna Bensch
+# Contact: jack.thorley1@gmail.com; jbt27@cam.ac.uk
 #----------------------------------------------------------------
 
 
