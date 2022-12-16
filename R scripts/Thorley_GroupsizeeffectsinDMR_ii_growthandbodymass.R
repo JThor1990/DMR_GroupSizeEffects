0ebf2b30d7474ddc27677f19c1510baa88cb860e
Jack Thorley <78469962+JThor1990@users.noreply.github.com>
2022-12-16 10:44:20 +0000

    Update Thorley_GroupsizeeffectsinDMR_ii_growthandbodymass.R

--- R scripts/Thorley_GroupsizeeffectsinDMR_ii_growthandbodymass.R
+++ R scripts/Thorley_GroupsizeeffectsinDMR_ii_growthandbodymass.R
@@ -1,11 +1,11 @@
 #----------------------------------------------------------------
-# No obvious benefit of group size in wild Damaraland mole-rats Fukomys damarensis
+# Damaraland mole-rat breeders do not rely on helpers for reproduction or survival
 #
 # ii- "Early life growth and adult body mass" 
 #
 # R script
-# Authors: Jack Thorley, Hanna Bensch, Markus Zöttl
-# Contact: jack.thorley1@gmail.com
+# Authors: Jack Thorley, Hanna Bensch
+# Contact: jack.thorley1@gmail.com; jbt27@cam.ac.uk
 #----------------------------------------------------------------
 
 library(tidyverse) ; library(nlme) ; library(MASS) ; library(multcomp) 
